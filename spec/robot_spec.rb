0907fc0ef8695db978dd7d4e1412cfeb8f362721
zrlstone <zak.stone@materialsmarket.com>
2025-08-01 18:17:26 +0100

    Adds test cases from task description

--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -15,5 +15,26 @@ RSpec.describe Robot do
 
       expect(robot.position).to eq([1, 1])
     end
+
+    context "command sequence N,E,S,W" do
+      it "moves the robot back to where it started" do
+        robot = Robot.new
+
+        robot.execute("N,E,S,W")
+
+        expect(robot.position).to eq([0, 0])
+      end
+    end
+
+    context "command sequence N,E,N,E,N,E,N,E" do
+      it "moves the robot to the correct position" do
+        robot = Robot.new
+
+        robot.execute("N,E,N,E,N,E,N,E")
+
+        expect(robot.position).to eq([4, 4])
+      end
+
+    end
   end
 end
